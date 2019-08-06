7dbd5aa002b4f2dac038e0ee00484e508f9293f0
Victor Wade <viwade@student.42.us.org>
2019-08-06 08:31:01 -0700

    updated submodules to use https instead of git protocol

--- source/projection/fdf_color_mode.c
+++ source/projection/fdf_color_mode.c
@@ -6,7 +6,7 @@
 /*   By: viwade <viwade@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/20 06:25:15 by viwade            #+#    #+#             */
-/*   Updated: 2019/07/21 17:36:06 by viwade           ###   ########.fr       */
+/*   Updated: 2019/07/21 18:08:13 by viwade           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #define M_R(c)	((0xFF0000 & c) >> 16)
 #define M_G(c)	((0xFF00 & c) >> 8)
 #define M_B(c)	(0xFF & c)
-#define V_SC(v)	(1/SQ(ABS(v) + 1))
+#define V_SC(v)	(1/(ABS(v) + 1))
 #define U_DR(v,c,z)	(uint)(((M_R(z)*V_SC(v)+(1.0-V_SC(v))*M_R(c)))) << 16
 #define U_DG(v,c,z)	(uint)(((M_G(z)*V_SC(v)+(1.0-V_SC(v))*M_G(c)))) << 8
 #define U_DB(v,c,z)	(uint)(((M_B(z)*V_SC(v)+(1.0-V_SC(v))*M_B(c))))
@@ -25,22 +25,25 @@
 #define V_M2R1(v)	(v)<0?V_M2C(v,VDAWN,VGREY):V_M2C(v,VWHTE,VGREY)
 #define V_M2R(v,r)	*(uint*)&v.col = V_M2R1(r.pos.z - v.pos.z);
 #define V_MD2(m,i,l,v,r)	{if(m==2){while(i++<l){V_M2R(v[i-1],r[i-1]);}}}
+#define V_SUB(m,i)	(m.ref_v[i].pos.z - m.v[i].pos.z)
 
 void	fdf_color_mode(fdf_t *o)
 {
 	size_t	i;
+	mesh_t	m;
 
 	i = 0;
 	o->modes = 2;
 	o->mode %= o->modes;
+	m = o->map.mesh;
 	if (!o->mode)
-		while (i++ < o->map.mesh.v_len)
-			ASNC(o->map.mesh.v[i - 1], o->map.mesh.ref_v[i - 1]);
+		while (i++ < m.v_len)
+			ASNC(m.v[i - 1], m.ref_v[i - 1]);
 	if (o->mode == 1)
-		while (i++ < o->map.mesh.v_len)
-			IF_E(o->map.mesh.v[i - 1].pos.z < 0,
-			ASNH(o->map.mesh.v[i - 1], o->map.mesh.v[i - 1].pos.z, VBLUE),
-			ASNH(o->map.mesh.v[i - 1], o->map.mesh.v[i - 1].pos.z, VDAWN));
-	V_MD2(o->mode, i, o->map.mesh.v_len, o->map.mesh.v, o->map.mesh.ref_v);
+		while (i++ < m.v_len)
+			IF_E(m.v[i - 1].pos.z < 0,
+			ASNH(m.v[i - 1], V_SUB(m, i - 1), VBLUE),
+			ASNH(m.v[i - 1], V_SUB(m, i - 1), VDAWN));
+	V_MD2(o->mode, i, m.v_len, m.v, m.ref_v);
 	fdf_redraw(o);
 }
--- source/runtime/fdf_run.c
+++ source/runtime/fdf_run.c
@@ -6,7 +6,7 @@
 /*   By: viwade <viwade@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/14 18:28:21 by viwade            #+#    #+#             */
-/*   Updated: 2019/07/21 17:26:14 by viwade           ###   ########.fr       */
+/*   Updated: 2019/07/21 17:57:58 by viwade           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,22 +20,22 @@
 #define K_SCLX(s,v)	{s.x *= s.x<0?K_SCL0(-(s.x),(1/v)):K_SCL0(s.x,v);}
 #define K_SCLY(s,v)	{s.y *= s.y<0?K_SCL0(-(s.y),(1/v)):K_SCL0(s.y,v);}
 #define K_SCLZ(s,v)	{s.z *= s.z<0?K_SCL0(-(s.z),(1/v)):K_SCL0(s.z,v);}
-#define K_MV2(k,t,r)	IF_C(r=k[KEY_ARROW_UP],K_MVU(t))
-#define K_MV1(k,t,r)	IF_C(r=k[KEY_ARROW_DOWN],K_MVD(t);K_MV2(k,t,r))
-#define K_MV0(k,t,r)	IF_C(r=k[KEY_ARROW_LEFT],K_MVL(t);K_MV1(k,t,r))
-#define K_MOV(k,t,r)	IF_C(r=k[KEY_ARROW_RIGHT],K_MVR(t);K_MV0(k,t,r))
-#define K_SC2(k,s,r)	IF_C(r=k[KEY_NUM_MINUS],K_SCLZ(s,(1/1.125)))
-#define K_SC1(k,s,r)	IF_E(r=k[KEY_NUM_PLUS],K_SCLZ(s,(1.125)),K_SC2(k,s,r))
-#define K_SC0(s)	 	IF_C(s.z!=S_Z0,s.z=S_Z0;return(1))
-#define K_SCL(k,s,r)	IF_C(r=k[KEY_0],K_SC0(s);K_SC1(k,s,r))
+#define K_MV2(k,t)	IF_C(k[KEY_ARROW_UP],K_MVU(t))
+#define K_MV1(k,t)	IF_C(k[KEY_ARROW_DOWN],K_MVD(t);K_MV2(k,t))
+#define K_MV0(k,t)	IF_C(k[KEY_ARROW_LEFT],K_MVL(t);K_MV1(k,t))
+#define K_MOV(k,t,r)	r=1;IF_C(k[KEY_ARROW_RIGHT],K_MVR(t);K_MV0(k,t))
+#define K_SC2(k,s)	IF_C(k[KEY_NUM_MINUS],K_SCLZ(s,(1/1.125)))
+#define K_SC1(k,s)	IF_E(k[KEY_NUM_PLUS],K_SCLZ(s,(1.125)),K_SC2(k,s))
+#define K_SC0(s)	IF_C(s.z!=S_Z0,s.z=S_Z0)
+#define K_SCL(k,s,r)	r=1;IF_E(k[KEY_0],K_SC0(s),K_SC1(k,s))
 
 int
 	fdf_run_loop(fdf_t *o)
 {
-	if (o->key[123] || o->key[124] || o->key[125] || o->key[126])
-		K_MOV(o->key, o->map.transform.translate, o->redraw);
-	if (o->key[29] || o->key[78] || o->key[69])
-		K_SCL(o->key, o->map.transform.scale, o->redraw);
+	IF_C(o->key[123] || o->key[124] || o->key[125] || o->key[126],
+		K_MOV(o->key, o->map.transform.translate, o->redraw));
+	IF_C(o->key[29] || o->key[78] || o->key[69],
+		K_SCL(o->key, o->map.transform.scale, o->redraw));
 	if (o->redraw)
 	{
 		fdf_redraw(o);
